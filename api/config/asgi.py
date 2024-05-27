2e1976b0a1af89738ca0080b3d5f378b205d000a
danwood1992 <gunnerwoody449@gmail.com>
2024-05-28 00:06:02 +0100

    connec4 sesssion model

--- api/config/asgi.py
+++ api/config/asgi.py
@@ -20,5 +20,10 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         your_app.routing.websocket_urlpatterns
+    #     )
+    # ),
     # Just HTTP for now. (We can add other protocols later.)
 })
---
+++ api/core/games/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from base.models import BaseModel
+import json
+# Create your models here.
+class Connect4Session(BaseModel):
+    """
+    A model to represent a Connect 4 game session.
+    """
+    # The board is represented as a 2D list of integers.
+    # 0 represents an empty space, 1 represents a red piece, and 2 represents a yellow piece.
+    board = models.TextField(default='[["", "", "", "", "", "", ""], ... ]')  # 6x7 grid
+    # The current player's turn.
+    current_player = models.CharField(max_startlength=1, default='R')
+    # The winner of the game.
+    winner = models.IntegerField()
+    # The number of moves that have been made in the game.
+    status = models.CharField(max_length=10, default='playing') 
+    
+    def get_board(self):
+        return json.loads(self.board)
+
+    def set_board(self, board):
+        self.board = json.dumps(board)
+        self.save()
