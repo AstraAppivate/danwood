ee7fdb9f6fa3058b29524956313189848def8725
danwood1992 <gunnerwoody449@gmail.com>
2024-06-06 16:12:00 +0100

    add host

--- api/config/settings.py
+++ api/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "realmdesk"),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost","tom.warfighter.danwood.io","*.warfighter.danwood.io"]
 
 ASGI_APPLICATION = 'config.asgi.application'
 
