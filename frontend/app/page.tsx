f336fd0f549b1d8fe02a352ccf34c71e284944c2
danwood1992 <gunnerwoody449@gmail.com>
2024-06-11 12:17:00 +0100

    styling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,38 @@
 'use client';
 
-import { Login } from '../components/login';
 
 
-export default function Dashboard() {
+export default function Home() {
   
   return (
-    <div >
-      <Login />
+    <div>
+      
+      <div className='text-center min-h-screen border rounded p-6 m-2 border-black'>
+    
+        <div className='rounded p-6 m-2 border border-black'>
+          
+          <div className='border rounded border-black'>
+            <h4>High scores</h4>
+              <ul>
+                <p>Connect 4</p>
+                <li>Dan: </li>
+                <li>Player 2: 50</li>
+              </ul>
+          </div>
+              
+          <div className='border rounded border-black'>
+            <h4>High scores</h4>
+              <ul>
+                <p>Connect 4</p>
+                <li>Dan: </li>
+                <li>Player 2: 50</li>
+              </ul>
+          </div>
+
+        </div>
+        <p className="text-bold"><span>Login</span> to track your scores</p>
+      </div>
+
     </div>
   );
 }
